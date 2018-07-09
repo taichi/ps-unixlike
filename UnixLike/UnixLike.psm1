d30469096aa7eb66e6f146cdd6c5b9a4d10139e7
taichi <ryushi@gmail.com>
2018-07-09 10:15:29 +0900

    remove duplicated exports

--- UnixLike/UnixLike.psm1
+++ UnixLike/UnixLike.psm1
@@ -2,4 +2,4 @@ function Get-Command-Source($name) {
     Get-Command $name | Select-Object -ExpandProperty Source
 }
 
-Export-ModuleMember -Function Get-Command-Source -Alias which
+New-Alias -Name which -Value Get-Command-Source
